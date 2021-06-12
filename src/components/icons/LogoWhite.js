eb3a85c124be6023439bc9c954f7900c7de6190f
Gabriel Borges <gabrielsmborges@gmail.com>
2021-06-13 00:46:39 +0100

    Adding dark mode compatibility and starting left desktop navbar

---
+++ src/components/icons/LogoWhite.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const LogoWhite = (props) =>
+    <svg {...props} viewBox="0 0 1320.48 575.76">
+    <path fill="none" stroke="#FFFFFF" strokeWidth="40" d="M88.39,75.32c126.98,141.57,253.96,283.14,380.94,424.71c254.31,0.04,508.63,0.08,762.94,0.12
+        C1105.55,358.92,978.83,217.69,852.12,76.46c-119.67-0.18-239.35-0.36-359.02-0.54C358.19,75.72,223.29,75.52,88.39,75.32z"/>
+    <path class="st0" fill="none" stroke="#FFFFFF" strokeWidth="40" d="M476.8,72.2c121.36,142.63,242.71,285.26,364.07,427.89"/>
+    </svg>
+
+
+    export default LogoWhite;
